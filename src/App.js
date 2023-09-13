0b9dfe59e02985e6ab6793da67f8247e70f45ab3
phreis <philippr01@yahoo.de>
2023-09-13 17:27:34 +0200

    Implement basic functionality w/o styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/** @jsxImportSource @emotion/react */
+// /** @jsxImportSource @emotion/react */
 // import { css } from '@emotion/react';
 import { saveAs } from 'file-saver';
 import { useState } from 'react';
